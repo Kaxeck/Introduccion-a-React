4dd57224ee0666823ad7caec78bd16de11731c50
Keith <keithalexis581@gmail.com>
2026-07-07 18:36:43 -0600

    Se agrega ejercicio de practica 2 Manejo de Estado

---
+++ Manejo-Estado/src/App.jsx
@@ -0,0 +1,12 @@
+import ListaCompras from "./ListaCompras";
+
+function App() {
+  return (
+    <div>
+      <h1>Mi Lista de Compras</h1>
+      <ListaCompras />
+    </div>
+  );
+}
+
+export default App;
